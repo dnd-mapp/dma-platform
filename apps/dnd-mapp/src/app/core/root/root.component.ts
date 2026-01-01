233dc7e4f6381f7b8fc00ce272858fd7037abb2b
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-02 00:37:35 +0100

    Remove empty stylesheet

--- apps/dnd-mapp/src/app/core/root/root.component.ts
+++ apps/dnd-mapp/src/app/core/root/root.component.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 @Component({
     selector: 'dma-root',
     templateUrl: './root.component.html',
-    styleUrl: './root.component.css',
     imports: [RouterModule],
 })
 export class RootComponent {}
