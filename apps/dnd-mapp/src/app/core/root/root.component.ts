e9e7d93d6eaf5a4e19cda4a56b5581c64bea03c9
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-02 00:38:10 +0100

    Set ChangeDetectionStrategy

--- apps/dnd-mapp/src/app/core/root/root.component.ts
+++ apps/dnd-mapp/src/app/core/root/root.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'dma-root',
     templateUrl: './root.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [RouterOutlet],
 })
 export class RootComponent {}
