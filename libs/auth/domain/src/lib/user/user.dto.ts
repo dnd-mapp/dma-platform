8f786d277cd2066b6376ee7788835e9e8b04a64d
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-04 20:50:56 +0100

    Add validation decorators

--- libs/auth/domain/src/lib/user/user.dto.ts
+++ libs/auth/domain/src/lib/user/user.dto.ts
@@ -1,13 +1,29 @@
+import { Exclude } from 'class-transformer';
+import { IsDate, IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class UserDto {
+    @IsNotEmpty()
+    @IsString()
     public id: string;
 
+    @MinLength(3)
+    @IsNotEmpty()
+    @IsString()
     public username: string;
 
+    @MinLength(12)
+    @IsNotEmpty()
+    @IsString()
+    @Exclude()
     public password: string;
 
+    @IsEmail()
+    @IsString()
     public email: string;
 
+    @IsDate()
     public createdAt: Date;
 
+    @IsDate()
     public updatedAt: Date;
 }
