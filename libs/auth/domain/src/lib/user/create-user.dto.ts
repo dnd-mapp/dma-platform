19d98ff784d4e057a2c20d7588ddfc6f0ee2f39d
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-04 19:08:54 +0100

    Fix typing

--- libs/auth/domain/src/lib/user/create-user.dto.ts
+++ libs/auth/domain/src/lib/user/create-user.dto.ts
@@ -1,6 +1,4 @@
 import { UserDto } from '@dnd-mapp/domain/auth';
 import { PickType } from '@nestjs/mapped-types';
 
-export const userProperties: (keyof UserDto)[] = ['username', 'password', 'email'] as const;
-
-export class CreateUserDto extends PickType(UserDto, userProperties) {}
+export class CreateUserDto extends PickType(UserDto, ['username', 'password', 'email'] as const) {}
