f9a96db38f7207481cf79af4f4e22244c233532f
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-04 20:55:48 +0100

    Enable whitelist during validation

--- libs/backend/core/src/lib/utils/validation.ts
+++ libs/backend/core/src/lib/utils/validation.ts
@@ -4,6 +4,7 @@ import { ValidatorOptions } from 'class-validator';
 export const validatorOptions: ValidatorOptions = {
     skipMissingProperties: false,
     stopAtFirstError: true,
+    whitelist: true,
 };
 
 export const validationPipeOptions: ValidationPipeOptions = {
