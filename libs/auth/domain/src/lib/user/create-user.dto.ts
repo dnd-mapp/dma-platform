c54979e21d0d26827e2af27e805047d5f5237f07
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-04 20:50:44 +0100

    Fix import

--- libs/auth/domain/src/lib/user/create-user.dto.ts
+++ libs/auth/domain/src/lib/user/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { UserDto } from '@dnd-mapp/domain/auth';
 import { PickType } from '@nestjs/mapped-types';
+import { UserDto } from './user.dto';
 
 export class CreateUserDto extends PickType(UserDto, ['username', 'password', 'email'] as const) {}
