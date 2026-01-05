5cd6df6d458a5ed26d9e08ee8ff4410adf5ed34c
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-05 22:57:15 +0100

    Forbid non whitelisted properties

--- libs/backend/core/src/lib/utils/validation.ts
+++ libs/backend/core/src/lib/utils/validation.ts
@@ -2,6 +2,7 @@ import { HttpStatus, ValidationPipeOptions } from '@nestjs/common';
 import { ValidatorOptions } from 'class-validator';
 
 export const validatorOptions: ValidatorOptions = {
+    forbidNonWhitelisted: true,
     skipMissingProperties: false,
     stopAtFirstError: true,
     whitelist: true,
