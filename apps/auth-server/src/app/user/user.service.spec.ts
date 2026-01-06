b758bbfdda5812cfe029ba035dc567122e0e5cbd
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-06 14:23:11 +0100

    Fix tests

--- apps/auth-server/src/app/user/user.service.spec.ts
+++ apps/auth-server/src/app/user/user.service.spec.ts
@@ -1,11 +1,13 @@
 import { provideMockDatabase } from '@dnd-mapp/backend/core/test';
 import { Test } from '@nestjs/testing';
+import { PasswordModule } from '../password';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
     async function setupTest() {
         const module = await Test.createTestingModule({
+            imports: [PasswordModule],
             providers: [UserService, UserRepository, provideMockDatabase()],
         }).compile();
 
