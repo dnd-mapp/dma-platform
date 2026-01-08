2d0143789451c45a8b66a111cfebecb33e3c9c0b
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-08 12:11:27 +0100

    Add util function to setup test environments

---
+++ libs/shared/ui/test/utils/setup-test-environment.ts
@@ -0,0 +1,37 @@
+import { ComponentHarness, HarnessLoader, HarnessQuery } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Type } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+interface SetupTestEnvironmentParams<H extends ComponentHarness, C = unknown> {
+    testComponent?: Type<C>;
+    harness?: HarnessQuery<H>;
+    imports?: unknown[];
+    providers?: unknown[];
+}
+
+export async function setupTestEnvironment<H extends ComponentHarness, C = unknown>(
+    params: SetupTestEnvironmentParams<H, C> = { imports: [], providers: [] },
+) {
+    TestBed.configureTestingModule({
+        imports: [...params.imports!, ...(params.testComponent ? [params.testComponent] : [])],
+        providers: [...params.providers!],
+    });
+
+    let fixture: ComponentFixture<C> | null = null;
+    let harnessLoader: HarnessLoader | null = null;
+    let harness: H | null = null;
+
+    if (params.testComponent) {
+        fixture = TestBed.createComponent(params.testComponent);
+        harnessLoader = TestbedHarnessEnvironment.loader(fixture);
+    }
+    if (params.harness && harnessLoader) {
+        harness = await harnessLoader.getHarness(params.harness);
+    }
+    return {
+        ...(fixture ? { fixture: fixture, component: fixture.componentInstance } : {}),
+        ...(harnessLoader ? { harnessLoader: harnessLoader } : {}),
+        ...(harness ? { harness: harness } : {}),
+    };
+}
