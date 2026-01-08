c632425e86b284c35def4da786ff23641c736606
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-08 16:45:52 +0100

    Add dotenv template file

--- e2e/auth-server/src/support/global-setup.ts
+++ e2e/auth-server/src/support/global-setup.ts
@@ -1,8 +1,8 @@
 import { waitForPortOpen } from '@nx/node/utils';
 
 export default async function globalSetup() {
-    const host = process.env['HOST'] || 'localhost';
-    const port = process.env['PORT'] ? Number(process.env['PORT']) : 4350;
+    const host = process.env['AUTH_SERVER_HOST'] || 'localhost';
+    const port = process.env['AUTH_SERVER_PORT'] ? Number(process.env['AUTH_SERVER_PORT']) : 4350;
 
     await waitForPortOpen(port, { host });
 }
--- e2e/auth-server/src/support/test-setup.ts
+++ e2e/auth-server/src/support/test-setup.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default async function testSetup() {
-    const host = process.env['HOST'] || 'localhost';
-    const port = process.env['PORT'] || 4350;
+    const host = process.env['AUTH_SERVER_HOST'] || 'localhost';
+    const port = process.env['AUTH_SERVER_PORT'] || 4350;
 
     axios.defaults.baseURL = `http://${host}:${port}`;
 }
