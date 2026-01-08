4f513555afa897538c96edfe4d072a908c14d7c4
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-08 21:37:00 +0100

    Use AuthService to initialize the authorization flow

--- libs/auth/ui/src/lib/login-button/login-button.component.ts
+++ libs/auth/ui/src/lib/login-button/login-button.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ButtonComponent } from '@dnd-mapp/shared/ui';
+import { AuthService } from '../auth';
 
 @Component({
     selector: 'dma-login-button',
@@ -8,7 +10,10 @@ import { ButtonComponent } from '@dnd-mapp/shared/ui';
     imports: [ButtonComponent],
 })
 export class LoginButtonComponent {
+    private readonly destroyRef = inject(DestroyRef);
+    private readonly authService = inject(AuthService);
+
     protected onLogin() {
-        console.warn('REDIRECTING TO LOGIN PAGE');
+        this.authService.authorize().pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
     }
 }
