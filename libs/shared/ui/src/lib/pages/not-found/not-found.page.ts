11767d824efb8363a51c4eac18094f88aa30b7e3
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-19 17:50:17 +0100

    Add NotFoundPage content

--- libs/shared/ui/src/lib/pages/not-found/not-found.page.ts
+++ libs/shared/ui/src/lib/pages/not-found/not-found.page.ts
@@ -1,9 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '@dnd-mapp/shared-ui';
+import { from } from 'rxjs';
 
 @Component({
     selector: 'dma-not-found',
     templateUrl: './not-found.page.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
+    host: {
+        '[class]': `'flex items-center h-full'`,
+    },
+    imports: [ButtonComponent],
 })
-export class NotFoundPage {}
+export class NotFoundPage {
+    private readonly router = inject(Router);
+    private readonly destroyRef = inject(DestroyRef);
+
+    protected onVisitHomePage() {
+        from(this.router.navigateByUrl('/')).pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+    }
+
+    protected onContactUs() {
+        location.href = 'https://github.com/dnd-mapp/dma-platform/issues';
+    }
+}
