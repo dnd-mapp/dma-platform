c5f37314e8eb27daa05008113573eb140beadf82
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-19 14:20:36 +0100

    Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default [
                 {
                     enforceBuildableLibDependency: true,
                     allowCircularSelfDependency: true,
-                    checkDynamicDependenciesExceptions: ['@dnd-mapp/shared-ui/pages/*'],
+                    checkDynamicDependenciesExceptions: ['@dnd-mapp/shared-ui'],
                     allow: ['^.*/eslint\\.config\\.mjs$'],
                     depConstraints: [
                         {
