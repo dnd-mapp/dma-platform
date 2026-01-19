7efc58170665c3d88325e931a07ecae5cf273e4d
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-19 16:18:15 +0100

    Add HomePage

--- apps/dnd-mapp/src/app/core/config/app.routes.ts
+++ apps/dnd-mapp/src/app/core/config/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
     {
+        path: '',
+        loadComponent: async () => (await import('../home')).HomePage,
+    },
+    {
         path: 'not-found',
         loadComponent: async () => (await import('@dnd-mapp/shared-ui/pages/not-found')).NotFoundPage,
     },
