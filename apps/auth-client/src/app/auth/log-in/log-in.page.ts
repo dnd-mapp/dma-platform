bd592ef36ff1a0dd2404e3250232433391b3fe9d
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-19 22:08:33 +0100

    Add LoginPage

---
+++ apps/auth-client/src/app/auth/log-in/log-in.page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-log-in',
+    templateUrl: './log-in.page.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export class LogInPage {}
