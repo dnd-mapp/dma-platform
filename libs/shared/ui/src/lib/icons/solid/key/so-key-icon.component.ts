b09cad6363b0572f8bba0ce29c3633dbe2bdb4d3
NoNamer777 <oscar.wellner@gmail.com>
2026-01-20 21:00:43 +0100

    Use a stylesheet to set icon styles

--- libs/shared/ui/src/lib/icons/solid/key/so-key-icon.component.ts
+++ libs/shared/ui/src/lib/icons/solid/key/so-key-icon.component.ts
@@ -3,10 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     selector: 'dma-icon[dma-so-key-icon]',
     templateUrl: './so-key-icon.component.svg',
+    styleUrl: '../../_icon.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {
-        '[class]': `'block w-[1.5rem]'`,
-    },
     imports: [],
 })
 export class SoKeyIconComponent {}
--- libs/shared/ui/src/lib/icons/solid/user/so-user-icon.component.ts
+++ libs/shared/ui/src/lib/icons/solid/user/so-user-icon.component.ts
@@ -3,10 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     selector: 'dma-icon[dma-so-user-icon]',
     templateUrl: './so-user-icon.component.svg',
+    styleUrl: '../../_icon.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {
-        '[class]': `'block w-[1.5rem]'`,
-    },
     imports: [],
 })
 export class SoUserIconComponent {}
