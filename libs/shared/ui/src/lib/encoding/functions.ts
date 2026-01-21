bb44f67c8f3537191ddda911c517fb0ee9f67c47
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-21 23:46:59 +0100

    Make base64 encoded strings URL safe

--- libs/shared/ui/src/lib/encoding/functions.ts
+++ libs/shared/ui/src/lib/encoding/functions.ts
@@ -2,7 +2,8 @@ export function base64(value: string, textEncoder: TextEncoder) {
     const bytes = textEncoder.encode(value);
     const binaryString = Array.from(bytes, (byte) => String.fromCharCode(byte)).join('');
 
-    return btoa(binaryString);
+    return btoa(binaryString).replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
+}
 
 export async function sha256(value: string, textEncoder: TextEncoder) {
     const bytes = textEncoder.encode(value);
