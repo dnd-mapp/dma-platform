cfe950182af4b37c36ba08f65adbf631cc357e8d
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-21 18:40:09 +0100

    Update linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,19 +20,29 @@ export default [
                     depConstraints: [
                         {
                             sourceTag: 'scope:dnd-mapp',
-                            onlyDependOnLibsWithTags: ['scope:dnd-mapp', 'scope:shared', 'scope:auth-ui'],
+                            onlyDependOnLibsWithTags: [
+                                'scope:dnd-mapp',
+                                'scope:shared-ui',
+                                'scope:auth-ui',
+                                'scope:shared-utils',
+                            ],
                         },
                         {
                             sourceTag: 'scope:auth-client',
-                            onlyDependOnLibsWithTags: ['scope:auth-client', 'scope:shared', 'scope:auth-ui'],
+                            onlyDependOnLibsWithTags: [
+                                'scope:auth-client',
+                                'scope:shared-ui',
+                                'scope:auth-ui',
+                                'scope:shared-utils',
+                            ],
                         },
                         {
                             sourceTag: 'scope:auth-server',
-                            onlyDependOnLibsWithTags: ['scope:auth-server', 'scope:shared'],
+                            onlyDependOnLibsWithTags: ['scope:auth-server', 'scope:shared-ui', 'scope:shared-utils'],
                         },
                         {
                             sourceTag: 'scope:auth-ui',
-                            onlyDependOnLibsWithTags: ['scope:shared'],
+                            onlyDependOnLibsWithTags: ['scope:shared-ui', 'scope:shared-utils'],
                         },
                         {
                             sourceTag: 'scope:shared-ui',
