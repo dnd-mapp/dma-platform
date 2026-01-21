a421fa3c3d7e944292838c6b233766c538a9f140
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-21 23:47:45 +0100

    Refactor base64 function to make TextEncoder optional when provided value is a Uint8Array

--- libs/shared/ui/src/lib/encoding/functions.ts
+++ libs/shared/ui/src/lib/encoding/functions.ts
@@ -1,6 +1,17 @@
-export function base64(value: string, textEncoder: TextEncoder) {
-    const bytes = textEncoder.encode(value);
-    const binaryString = Array.from(bytes, (byte) => String.fromCharCode(byte)).join('');
+export function base64(value: Uint8Array<ArrayBuffer>): string;
+export function base64(value: string, textEncoder: TextEncoder): string;
+export function base64(value: string | Uint8Array<ArrayBuffer>, textEncoder?: TextEncoder) {
+    let byteArray: Uint8Array<ArrayBuffer>;
+
+    if (typeof value === 'string') {
+        if (textEncoder === undefined) {
+            throw new Error(`A TextEncoder must be provided when "value" is a "string".`);
+        }
+        byteArray = textEncoder.encode(value);
+    } else {
+        byteArray = value;
+    }
+    const binaryString = Array.from(byteArray, (byte) => String.fromCharCode(byte)).join('');
 
     return btoa(binaryString).replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
 }
