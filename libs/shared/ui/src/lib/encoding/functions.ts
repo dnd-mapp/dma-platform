4c332b4d238a40ee51ce00d688b5ace0c30d3881
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-21 23:46:12 +0100

    Add function to create a SHA-256 hash

--- libs/shared/ui/src/lib/encoding/functions.ts
+++ libs/shared/ui/src/lib/encoding/functions.ts
@@ -3,4 +3,8 @@ export function base64(value: string, textEncoder: TextEncoder) {
     const binaryString = Array.from(bytes, (byte) => String.fromCharCode(byte)).join('');
 
     return btoa(binaryString);
+
+export async function sha256(value: string, textEncoder: TextEncoder) {
+    const bytes = textEncoder.encode(value);
+    return await crypto.subtle.digest('SHA-256', bytes);
 }
