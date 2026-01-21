133b54d0ced57d9fce64e89a9c8753b2de1441e0
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-21 22:21:24 +0100

    Generate and store Code Verfier

--- libs/auth/ui/src/lib/auth/auth.service.ts
+++ libs/auth/ui/src/lib/auth/auth.service.ts
@@ -1,10 +1,23 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { base64, StorageKeys, StorageService, TEXT_ENCODER } from '@dnd-mapp/shared-ui';
+import { nanoid } from 'nanoid';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    private readonly textEncoder = inject(TEXT_ENCODER);
+    private readonly storageService = inject(StorageService);
+
     public readonly authenticated = signal(false);
 
     public authorize() {
+        const codeVerifier = this.generateCodeVerifier();
+
+        this.storageService.setItem(StorageKeys.CODE_VERIFIER, codeVerifier);
+
         location.href = 'https://localhost.auth.dndmapp.dev:4350/authorize';
+    }
+
+    private generateCodeVerifier() {
+        return base64(nanoid(), this.textEncoder);
     }
 }
