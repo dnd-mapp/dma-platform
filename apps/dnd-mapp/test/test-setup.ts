3e61c8b3867a81e6aa933bfeecdbd58ace3eae50
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-24 15:25:12 +0100

    Set up msw during testing

--- apps/dnd-mapp/test/test-setup.ts
+++ apps/dnd-mapp/test/test-setup.ts
@@ -2,5 +2,21 @@
 import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
+import { initializeMockServiceWorker, clientHandlers } from '@dnd-mapp/shared-ui/test';
+import { authServerHandlers } from '@dnd-mapp/auth-ui/test';
+
+const { startWorker, resetWorker, stopWorker } = initializeMockServiceWorker(...clientHandlers, ...authServerHandlers);
+
+beforeAll(async () => {
+    await startWorker();
+});
+
+beforeEach(() => {
+    resetWorker();
+});
+
+afterAll(() => {
+    stopWorker();
+});
 
 setupTestBed({ browserMode: true, zoneless: true });
