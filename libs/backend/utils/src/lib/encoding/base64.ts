d6f9d454b858199c96a05240d08bc61bc2a51c58
NoNamer777 <oscar.wellner@gmail.com>
2026-01-24 12:41:44 +0100

    Add base64 decode function

--- libs/backend/utils/src/lib/encoding/base64.ts
+++ libs/backend/utils/src/lib/encoding/base64.ts
@@ -2,3 +2,8 @@ export function toBase64(value: string) {
     const buffer = Buffer.from(value, 'utf8');
     return buffer.toString('base64url');
 }
+
+export function fromBase64(value: string) {
+    const buffer = Buffer.from(value, 'base64url');
+    return buffer.toString('utf8');
+}
