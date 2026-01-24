8567ff10892f8ce50d554010850849b765c105f1
NoNamer777 <oscar.wellner@gmail.com>
2026-01-24 12:42:34 +0100

    Add authServerBaseUrl and make clientId optional

--- apps/auth-client/src/app/core/config/app.config.ts
+++ apps/auth-client/src/app/core/config/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientConfig } from '@dnd-mapp/shared-ui';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+    providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes), provideClientConfig()],
 };
