c128bb72069b7f3812c017213c7fac8b77e1b925
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-26 13:20:50 +0100

    Add server-side function to create SHA-256 hash

--- libs/backend/utils/src/lib/hashing.ts
+++ libs/backend/utils/src/lib/hashing.ts
@@ -1,4 +1,5 @@
 import { argon2i, hash, verify } from 'argon2';
+import { createHash } from 'crypto';
 
 export async function hashPassword(password: string, pepper: string) {
     return await hash(password, { timeCost: 8, type: argon2i, secret: Buffer.from(pepper) });
@@ -6,4 +7,8 @@ export async function hashPassword(password: string, pepper: string) {
 
 export async function verifyPassword(password: string, passwordHash: string, pepper: string) {
     return await verify(passwordHash, password, { secret: Buffer.from(pepper) });
+}
+
+export function sha256(value: string) {
+    return createHash('sha256').update(value).digest('base64url');
 }
