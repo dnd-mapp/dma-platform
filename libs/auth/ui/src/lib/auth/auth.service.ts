443f469863913d9b1328c15a20dbcea129712f5c
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-27 15:42:26 +0100

    Add idToken signal

--- libs/auth/ui/src/lib/auth/auth.service.ts
+++ libs/auth/ui/src/lib/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { type GetTokenDto, hasAuthCodeGrant, type LoginDto, TokenGrantType } from '@dnd-mapp/auth-domain';
 import { base64, ConfigService, sha256, StorageKeys, StorageService, TEXT_ENCODER } from '@dnd-mapp/shared-ui';
 import { jwtDecode, JwtPayload } from 'jwt-decode';
@@ -26,7 +26,9 @@ export class AuthService {
 
     public readonly authenticated = signal(false);
 
-    private readonly accessToken = signal<string | null>(null);
+    public readonly accessToken = signal<string | null>(null);
+
+    public readonly idToken = signal<DmaJwtIdTokenPayload | null>(null);
 
     public authorize() {
         const codeVerifier = this.generateCodeVerifier();
@@ -125,6 +127,7 @@ export class AuthService {
         if (nonce) {
             this.storageService.removeItem(StorageKeys.ID_NONCE);
         }
+        this.idToken.set(token);
         return true;
     }
 }
