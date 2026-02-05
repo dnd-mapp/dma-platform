2ac798b5767432c05e139a05bb16c79d0c3f18f5
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-05 20:55:42 +0100

    Return `undefined` when no cookies are send in the request

--- apps/auth-server/src/app/core/decorators/cookies.decorator.ts
+++ apps/auth-server/src/app/core/decorators/cookies.decorator.ts
@@ -5,6 +5,7 @@ export const Cookies = createParamDecorator((cookieName: string, context: Execut
     const request = context.switchToHttp().getRequest<FastifyRequest>();
     const cookies = request.cookies;
 
+    if (Object.keys(cookies).length === 0) return undefined;
     if (cookieName && cookies[cookieName]) return request.unsignCookie(cookies[cookieName]);
     return cookies;
 });
