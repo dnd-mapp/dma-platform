ad04a36e39a00704a00dac2afd19077c43cad291
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-06 11:57:34 +0100

    Add mock handler for the token end-point

--- libs/auth/ui/test/mock/auth-server/handlers/auth.handlers.ts
+++ libs/auth/ui/test/mock/auth-server/handlers/auth.handlers.ts
@@ -1,3 +1,13 @@
-import { RequestHandler } from 'msw';
+import { delay, http, HttpResponse, RequestHandler } from 'msw';
+import { BASE_URL } from '../constants';
 
-export const authHandlers: RequestHandler[] = [];
+export const authHandlers: RequestHandler[] = [
+    http.post(`${BASE_URL}/token`, async () => {
+        await delay();
+
+        return HttpResponse.json({
+            accessToken: 'access-token',
+            idToken: 'idToken',
+        });
+    }),
+];
