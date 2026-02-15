46009ce70c39c3fa4f9f7bb54038f6be64512164
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-15 20:30:54 +0100

    Feat: Show notifications

---
+++ libs/shared/ui/test/harnesses/notifications/notification.harness.ts
@@ -0,0 +1,5 @@
+import { ComponentHarness } from '@angular/cdk/testing';
+
+export class NotificationHarness extends ComponentHarness {
+    public static readonly hostSelector = 'dma-notification';
+}
