b5356e5362b4d9efaa798515fe1926b7e850c994
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-15 23:31:55 +0100

    Fix: Remove remaining tests

--- libs/shared/ui/test/harnesses/notifications/notification.harness.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentHarness } from '@angular/cdk/testing';
-
-export class NotificationHarness extends ComponentHarness {
-    public static readonly hostSelector = 'dma-notification';
-}
