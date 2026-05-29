b7b2bbb4d720c72d2bcb17ffe508208074b863a2
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-05-30 00:28:25 +0200

    fix(eslint): Add TS parser to e2e lint config

--- e2e/realm/eslint.config.mjs
+++ e2e/realm/eslint.config.mjs
@@ -1,10 +1,12 @@
 import baseConfig from '@dnd-mapp/eslint-config';
+import tseslint from 'typescript-eslint';
 
 export default [
     ...baseConfig,
     {
         files: ['**/*.ts'],
         languageOptions: {
+            parser: tseslint.parser,
             parserOptions: {
                 projectService: true,
                 tsconfigRootDir: import.meta.dirname,
