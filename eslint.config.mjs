65549e20008877df24722e1b231ed8027b2f6add
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-05-30 00:29:48 +0200

    chore: Format configs and add e2e run configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,15 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     {
-        ignores: ['apps/realm/**', 'e2e/**', 'packages/eslint-config/**', 'dist/**', 'coverage/**', 'reports/**', '.angular/**'],
+        ignores: [
+            'apps/realm/**',
+            'e2e/**',
+            'packages/eslint-config/**',
+            'dist/**',
+            'coverage/**',
+            'reports/**',
+            '.angular/**',
+        ],
     },
     {
         files: ['**/*.{js,mjs,cjs}'],
