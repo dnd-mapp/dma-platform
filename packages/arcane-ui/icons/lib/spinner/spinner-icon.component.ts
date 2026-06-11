00824d9a809a9296ab900435e14df70618046965
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-06-11 09:28:47 +0200

    feat(arcane-ui): Implement DmaSpinnerIconComponent

---
+++ packages/arcane-ui/icons/lib/spinner/spinner-icon.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+export type IconSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+@Component({
+    selector: 'dma-icon-spinner',
+    templateUrl: `./spinner-icon.component.svg`,
+    styleUrl: './spinner-icon.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        'aria-hidden': 'true',
+        '[class]': 'size()',
+    },
+})
+export class DmaSpinnerIconComponent {
+    public readonly size = input<IconSize>('md');
+}
